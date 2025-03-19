449e116e4f3e2c0770b82bbfd29427f7f5cf680b
Abhilash Tekumatla <labhi1442@gmail.com>
2025-03-19 00:01:18 +0000

    dashboard

--- app/app.js
+++ app/app.js
@@ -31,14 +31,14 @@ app.get("/contact", function(req, res) {
     res.render("contact")
 });
 
-// Create a route for testing the db
-app.get("/db_test", function(req, res) {
-    // Assumes a table called test_table exists in your database
-    sql = 'select * from test_table';
-    db.query(sql).then(results => {
-        console.log(results);
-        res.send(results)
-    });
+app.get("/dashboard", function (req, res) {
+    let sql = "SELECT * FROM fitness_records";
+    
+    db.query(sql)
+        .then(results => {
+            res.render("dashboard", { records: results }); // Pass records to Pug template
+        })
+
 });
 
 
